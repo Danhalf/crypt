60591719df2e36c2037e3a15d79394712757e2e8
Danhalf <danhel@rocketmail.com>
2021-05-08 16:28:55 +0300

    add data to local storage

--- src/App.vue
+++ src/App.vue
@@ -138,10 +138,33 @@ export default {
       graph: [],
       allCoins: [],
       findCoins: [],
-      appLoaded: true,
+      appLoaded: false,
     };
   },
+
+  created() {
+    this.appLoaded = false;
+    const tickersData = localStorage.getItem('cryptonomicon-list');
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+  },
+
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=046bb019f05540522ba573c299b93ce437555d72d2fabf2b52942a7af2f31132`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === tickerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name === tickerName) this.graph.push(data.USD);
+        if (this.graph.length > 80) this.graph.shift();
+      }, 3000);
+    },
     add() {
       const currentTicker = {
         name: this.ticker.toUpperCase(),
@@ -149,15 +172,8 @@ export default {
       };
       this.alreadyTicker = false;
       this.tickers.some((elem) => elem.name == currentTicker.name) ? (this.alreadyTicker = true) : this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=046bb019f05540522ba573c299b93ce437555d72d2fabf2b52942a7af2f31132`
-        );
-        const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name === currentTicker.name) this.graph.push(data.USD);
-        if (this.graph.length > 80) this.graph.shift();
-      }, 3000);
+      this.subscribeToUpdates(currentTicker.name);
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
       this.ticker = '';
     },
 
@@ -192,7 +208,7 @@ export default {
 
   mounted() {
     window.addEventListener('load', async () => {
-      // this.appLoaded = true;
+      this.appLoaded = true;
       const fetchCoins = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true');
       const data = await fetchCoins.json();
       this.allCoins = data.Data;
